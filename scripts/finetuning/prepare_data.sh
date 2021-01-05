cf2dde9516dfbc79ff16c16c0825b08f2b67b7af
Harveen Singh Chadha <30959215+harveenchadha@users.noreply.github.com>
2021-01-05 11:28:12 +0530

    Harveen | Updating Data Prep script

--- scripts/finetuning/prepare_data.sh
+++ scripts/finetuning/prepare_data.sh
@@ -1,16 +1,27 @@
-train_path="/home/harveen.chadha/_/english-asr-challenge/data/training"
+
+### Values to change -start ###
+
 prep_scripts="/home/harveen.chadha/common_scripts/prep_scripts"
 data_path="/home/jupyter/english_asr_challenge/train_NPTEL_IITM/"
-dataset_type="train"
 valid_percentage=0.04
 
-python ${prep_scripts}/manifest.py ${data_path} --dest ${train_path} --ext wav --train-name train --valid-percent ${valid_percentage} --jobs -1
+### Values to change end ###
+
+
+dir=$PWD
+parentdir="$(dirname "$dir")"
+parentdir="$(dirname "$parentdir")"
+
+destination_path=$parentdir'/data/finetuning'
+
+
+python ${prep_scripts}/manifest.py ${data_path} --dest ${destination_path} --ext wav --train-name train --valid-percent ${valid_percentage} --jobs -1
 echo "Manifest Creation Done"
 
-python ${prep_scripts}/labels.py --jobs 64 --tsv ${train_path}/train.tsv --output-dir ${train_path} --output-name train --txt-dir ${data_path}
-python ${prep_scripts}/labels.py --jobs 64 --tsv ${train_path}/valid.tsv --output-dir ${train_path} --output-name valid --txt-dir ${data_path}
+python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/train.tsv --output-dir ${destination_path} --output-name train --txt-dir ${data_path}
+python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/valid.tsv --output-dir ${destination_path} --output-name valid --txt-dir ${data_path}
 
 echo "Word file generated"
 
-python ${prep_scripts}/dict_and_lexicon_maker.py --wrd ${train_path}/train.wrd --lexicon ${train_path}/lexicon.lst --dict ${train_path}/dict.ltr.txt
+python ${prep_scripts}/dict_and_lexicon_maker.py --wrd ${destination_path}/train.wrd --lexicon ${destination_path}/lexicon.lst --dict ${destination_path}/dict.ltr.txt
 echo "Dict file generated"
