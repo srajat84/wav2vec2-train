254bd12796353f80588ca7992f2b851ad5fbb4c9
Harveen <harveen>
2021-01-07 17:58:12 +0000

    Harveen | XLSR Finetuning 6 WER

--- scripts/finetuning/prepare_data.sh
+++ scripts/finetuning/prepare_data.sh
@@ -2,8 +2,8 @@
 ### Values to change -start ###
 
 prep_scripts="/home/anirudh/common_scripts/prep_scripts"
-data_path="/home/anirudh/2020_kn_1/finetune_data_ldcil_ka"
-valid_percentage=0.04
+data_path="/home/anirudh/2020_kn_1/speaker_split_ldcil/test_files"
+valid_percentage=0
 
 ### Values to change end ###
 
@@ -15,13 +15,13 @@ parentdir="$(dirname "$parentdir")"
 destination_path=$parentdir'/data/finetuning'
 
 
-python ${prep_scripts}/manifest.py ${data_path} --dest ${destination_path} --ext wav --train-name train --valid-percent ${valid_percentage} --jobs -1
+python ${prep_scripts}/manifest.py ${data_path} --dest ${destination_path} --ext wav --train-name valid --valid-percent ${valid_percentage} --jobs -1
 echo "Manifest Creation Done"
 
-python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/train.tsv --output-dir ${destination_path} --output-name train --txt-dir ${data_path}
+#python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/train.tsv --output-dir ${destination_path} --output-name train --txt-dir ${data_path}
 python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/valid.tsv --output-dir ${destination_path} --output-name valid --txt-dir ${data_path}
 
 echo "Word file generated"
 
-python ${prep_scripts}/dict_and_lexicon_maker.py --wrd ${destination_path}/train.wrd --lexicon ${destination_path}/lexicon.lst --dict ${destination_path}/dict.ltr.txt
+#python ${prep_scripts}/dict_and_lexicon_maker.py --wrd ${destination_path}/train.wrd --lexicon ${destination_path}/lexicon.lst --dict ${destination_path}/dict.ltr.txt
 echo "Dict file generated"
--- scripts/inference/infer.sh
+++ scripts/inference/infer.sh
@@ -3,7 +3,7 @@
 
 
 fairseq_path='/opt/fairseq'
-validation_dataset='openslr_google'
+validation_dataset='ldcil_big_folder'
 
 dir=$PWD
 parentdir="$(dirname "$dir")"
@@ -14,8 +14,8 @@ data_path=${parentdir}'/data/dev/'${validation_dataset}
 checkpoint_path=${parentdir}'/checkpoints/finetuning/checkpoint_best.pt'
 result_path=${parentdir}'/results/'${validation_dataset}
 subset='valid'
-w2l_decoder='kenlm'
-lm=1 # 0 for viterbi and 1 for kenlm
+w2l_decoder='viterbi'
+lm=0 # 0 for viterbi and 1 for kenlm
 beam=128
 
 if [ "${lm}" = 0 ]; then
@@ -30,7 +30,7 @@ else
 
 	python ${fairseq_path}/examples/speech_recognition/infer.py ${data_path} --task audio_pretraining \
 	--nbest 1 --path ${checkpoint_path} --gen-subset $subset --results-path ${result_path}_kenlm --w2l-decoder ${w2l_decoder} --lm-model ${parentdir}/data/lm/lm.binary \
-	--lm-weight 2 --word-score -1 --sil-weight 0 --criterion ctc --labels ltr --max-tokens 4000000 --lexicon ${parentdir}/data/finetuning/lexicon.lst \
+	--lm-weight 2 --word-score -1 --sil-weight 0 --criterion ctc --labels ltr --max-tokens 6000000 --lexicon ${parentdir}/data/finetuning/lexicon.lst \
 	--post-process letter --beam ${beam}
 
 fi	
